c31863067e0c4ca1b53a3e453cbc63709dd844ab
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-02-08 09:05:49 +0530

    added

--- charusat_practical_CPP/practical_2_4.cpp
+++ charusat_practical_CPP/practical_2_4.cpp
@@ -24,7 +24,7 @@ public:
     }
     void add_stock();
     void sell_stock();
-    void total_stock();
+    void total_stock(int n, Inventory_Management_System I[]);
     void display();
 };
 void Inventory_Management_System::add_stock()
@@ -49,60 +49,105 @@ void Inventory_Management_System::sell_stock()
         cout << "Insufficient Stock!" << endl;
     }
 }
-
-void Inventory_Management_System::total_stock(){
-    cout << "Total Stock: " << Quantity_in_stock << endl;
+void Inventory_Management_System::total_stock(int n, Inventory_Management_System I[])
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Item " << i + 1 << ": ";
+       
+        sum = sum + I[i].Quantity_in_stock;
+    }
+    cout << "Total Stock Count: " << sum << endl;
 }
-
-void Inventory_Management_System::display(){
-    cout<<"________________________________________________________";
+void Inventory_Management_System::display()
+{
+    cout << "*********************************************" << endl;
     cout << "Unique Item ID: " << Unique_item_ID << endl;
     cout << "Item Name: " << Item_name << endl;
     cout << "Price: " << Price << endl;
     cout << "Quantity in Stock: " << Quantity_in_stock << endl;
 }
-int main(){
-    /*    Inventory_Management_System I[3]={
-        Inventory_Management_System("12ABC", "Laptop", 90.12, 10),
-        Inventory_Management_System(),
-        Inventory_Management_System("EF346", "Mouse", 29, 50)
-    };
-    for(int i=0; i<3; i++)
-    {
-        I[i].display();
-        cout << endl;
-    }
-    */
-
+int main()
+{
+    /* Inventory_Management_System I[3]={
+         Inventory_Management_System("12ABC", "Laptop", 90.12, 10),
+         Inventory_Management_System(),
+         Inventory_Management_System("EF346", "Mouse", 29, 50)
+     };
+     for(int i=0; i<3; i++)
+     {
+         I[i].display();
+         cout << endl;
+     }*/
     int n;
-    cout<<"Enter Nubers of Items: ";
-    cin>>n;
-
+    cout << "How many items do you want to manage? ";
+    cin >> n;
     Inventory_Management_System I[n];
+    cout << "Items created successfully!" << endl;
+    cout << "1. Add Stock" << endl;
+    cout << "2. Sell Stock" << endl;
+    cout << "3. Display Inventory" << endl;
+    cout << "4. Total Stock Count" << endl;
+    cout << "5. Add details of an item" << endl;
+    cout << "6. Exit" << endl;
+    int choice;
+    do
+    {
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter index of item to add stock: ";
+            int i;
+            cin >> i;
+            if (i >= 0 && i < n)
+                I[i].add_stock();
 
-    for(int i=1; i<=n; i++){
-        string Unique_item_ID;
-        string Item_name;
-        float Price;
-        int Quantity_in_stock;
+            break;
+        case 2:
+            cout << "Enter index of item to sell stock: ";
+            int j;
+            cin >> j;
+            if (j >= 0 && j < n)
+                I[j].sell_stock();
+            else
+                cout << "Invalid index!" << endl;
+            break;
+        case 3:
+            for (int i = 0; i < n; i++)
+            {
+                I[i].display();
+                cout << endl;
+            }
+            break;
+        case 4:
+             I[n-1].total_stock(n, I);
+            break;
+        case 5:
+        {
+            string id, name;
+            float price;
+            int quantity;
+            cout << "Enter Unique Item ID: ";
+            cin >> id;
+            cout << "Enter Item Name: ";
+            cin >> name;
+            cout << "Enter Price: ";
+            cin >> price;
+            cout << "Enter Quantity in Stock: ";
+            cin >> quantity;
+            I[n - 1] = Inventory_Management_System(id, name, price, quantity);
+        }
+        break;
+        case 6:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+        }
+    } while (choice != 5);
 
-        cout<<"Enter Unique_item_ID: ";
-        cin>>Unique_item_ID;
-
-        cout<<"Enter Item_name: ";
-        cin>>Item_name;
-
-        cout<<"Enter price: ";
-        cin>>Price;
-
-        cout<<"Enter Quantity_in_stock: ";
-        cin>>Quantity_in_stock;
-
-       // I[i].display();
-    }
-
-    for(int i=1; i<=n; i++){
-        I[i].display();
-    }
     return 0;
 }
