48287b3604d15da721b82c6a72f1a7a3d3fa377d
Hirapara Nirbhay <hnhpatel66@gmail.com>
2026-02-21 15:15:56 +0530

    Add files via upload

---
+++ Object_Oriented_Programing/polimorphism.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Polymorphism
+    // - one name and multiple forms
+    // - eg. Function overloading, operator overloading
+    // - eg. Virtual functions
+
+    /*
+    Polymorphism in C++ can be of two types:
+
+    1. Compile time polymorphism--->Arli Binding
+       Compile time polymorphism in C++ is achieved using:
+       1.1 - Function overloading
+       1.2 - Operator Overloading
+
+    2. Run time polymorphism---> Let Binding
+       Run time polymorphism in C++ is achieved using:
+       2.1 - Virtual functions
+    */
+
+    return 0;
+}
