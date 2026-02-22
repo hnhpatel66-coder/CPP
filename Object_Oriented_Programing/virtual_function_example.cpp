24fcbfbf56866071613194f8b98ef8b8e31b8ea7
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-02-22 18:46:07 +0530

    fs

--- Object_Oriented_Programing/virtual_function_example.cpp
+++ Object_Oriented_Programing/virtual_function_example.cpp
@@ -73,3 +73,10 @@ int main(){
 
     return 0;
 }
+
+// Rules for virtual functions
+// 1. They cannot be static
+// 2. They are accessed by object pointers
+// 3. Virtual functions can be a friend of another class
+// 4. A virtual function in base class might not be used.
+// 5. If a virtual function is defined in a base class, there is no necessity of redefining it in the derived class
