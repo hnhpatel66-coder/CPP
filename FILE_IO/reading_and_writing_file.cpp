f7e019dc99697108a2d457bc6500659db41695fb
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-02-23 18:48:57 +0530

    qdq

---
+++ FILE_IO/reading_and_writing_file.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string st;
+    cout<<"Enter your name: ";
+    getline(cin,st);
+    string st2;
+    //opening file using constructor and writing it
+    ofstream out("sample.txt"); // write operation
+    out<<st;
+
+    cin.ignore(); // to ignore the newline character left in the input buffer
+
+    //opening file using constructor and reading it
+    ifstream in("sample1.txt"); // read operation
+    // in>>st2; // this will only read the first word
+    getline(in,st2); // this will read the whole line
+    cout<<st2<<endl;
+
+
+    return 0;
+}
