6ec8d6e65bff24fea5e3c7bc470e514cbe85f8c3
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-04-05 10:08:39 +0530

    comitt practical 7.3

--- charusat_practical_CPP/practical_7_3.cpp
+++ charusat_practical_CPP/practical_7_3.cpp
@@ -8,34 +8,43 @@ class cel{
         float celcius;
         
     public:
+        cel():celcius(0){}
         cel(float c):celcius(c){}
 
-        void fahrenhit(){
-            float f=((celcius*9/5)+32);
-            cout << "Temperature in Fahrenheit: " << f << endl;
-        }
+        // void fahrenhit(){
+        //     float f=((celcius*9/5)+32);
+        //     cout << "Temperature in Fahrenheit: " << f << endl;
+        // }
 
         bool operator ==(cel temp) {
         return (celcius == temp.celcius);
-    }
+        }
+
+        operator far();
 };
 
 class far{
     private:
         float fahrenheit;
     public:
+        far():fahrenheit(0){}
         far(float f):fahrenheit(f){}
 
-        void celcius(){
-            float c=((fahrenheit-32)*5/9);
-            cout << "Temperature in Celcius: " << c << endl;
-        }
+        // void celcius(){
+        //     float c=((fahrenheit-32)*5/9);
+        //     cout << "Temperature in Celcius: " << c << endl;
+        // }
 
         operator cel(){
             float c = (fahrenheit - 32) * 5/9;
             return cel(c);
         }
 };
+
+cel::operator far() {
+    float f = (celcius * 9/5) + 32;
+    return far(f);
+}
 
 int main() {
     float n1, n2;
@@ -48,8 +57,11 @@ int main() {
     cel c(n1);
     far f(n2);
 
-    c.fahrenhit();
-    f.celcius();
+    far f1= c;
+    cel c1= f;
+
+    // c.fahrenhit();
+    // f.celcius();
 
     if(c == f){
         cout << "Temperatures are equal." << endl;
