91bc06d71c07228dc13021c87084113e686981fc
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-04-07 09:51:17 +0530

    comitt practical 4.3

--- charusat_practical_CPP/practical_4_3.cpp
+++ charusat_practical_CPP/practical_4_3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-int n,m;
+int n,m, i=0, j=0, k=0;;
 
     cout<<"Enter size of first array: ";
     cin>>n;
@@ -15,21 +15,17 @@ int n,m;
         return 0;
     }
 
-
     int *arr1 = new int[n];
     int *arr2 = new int[m];
-
-    for(int i=0; i<n; i++){
-        cin>>arr1[i];
-    }
-
-    for(int i=0; i<n; i++){
-        cin>>arr2[i];
-    }
-
     int *merg= new int[m+n];
 
-    int i=0, j=0, k=0;
+    cout << "Enter " <<n<< " sorted elements for array 1: ";
+    for (int i=0; i<n; i++)
+        cin>>arr1[i];
+    
+    cout << "Enter " <<m<< " sorted elements for array 2: ";
+    for (int i=0; i<m; i++)
+        cin>>arr2[i];
 
     while(i<n && j<m){
         if(arr1[i]<=arr2[j]){
@@ -47,7 +43,7 @@ int n,m;
         merg[k++]=arr2[j++];
     }
 
-    for(int i=0; i<m+n; i++){
+    for(int i=0; i<n+m; i++){
         cout<<merg[i]<<" ";
     }
 
