6447dcdc0a3a609a49ac6d78d4bb1214286aa290
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-04-07 21:48:01 +0530

    comitt  practical 4.2

---
+++ charusat_practical_CPP/practical_8_2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class  NegativeNumberException{
+    public:
+        void showError() {
+        cout << "Error: Negative number entered! Square root not possible." << endl;
+    }
+};
+
+int main(){
+    double n;
+    cout<<"Enter Value of n: ";
+    cin>>n;
+
+    try{// risky code
+        // Check for negative number
+        if(n < 0) {
+            throw NegativeNumberException(); // throw exception
+        }
+        else{// Calculate square root
+        double result = sqrt(n);
+        cout << "Square root = " << result << endl;
+        }
+    }
+
+    catch (NegativeNumberException a) {
+        a.showError(); // handle exception
+    }
+    return 0;
+}
