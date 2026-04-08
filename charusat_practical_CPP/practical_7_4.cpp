c9e6046299bb73ab049c24538a5af9a08f042f41
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-04-08 10:28:02 +0530

    comitt  practical 7.4

--- charusat_practical_CPP/practical_7_4.cpp
+++ charusat_practical_CPP/practical_7_4.cpp
@@ -17,6 +17,13 @@ class Rectangle: public Shape{
         void area(){
             cout<<"Area of Rectangle: "<<length*breadth<<endl;
         }
+        void RectangleInput(){
+            cout<<"Enter Length: ";
+            cin>>length;
+
+            cout<<"Enter Breadth: ";
+            cin>>breadth;
+        }
 };
 
 class Circle: public Shape{
@@ -30,15 +37,71 @@ class Circle: public Shape{
         void area(){
             cout<<"Area of Circle: "<<3.14*redius*redius<<endl;
         }
+        void CircleInput(){
+            cout<<"Enter Redius: ";
+            cin>>redius;
+        }
 };
 
 int main(){
 
-    Rectangle r1(3.3 , 5.5);
-    Circle c1(3.9);
+    // Rectangle r1(3.3 , 5.5);
+    // Circle c1(3.9);
 
-    r1.area();
-    c1.area();
+    // r1.area();
+    // c1.area();
 
+    int n,m;
+    cout<<"Enter the Number of Circle: ";
+    cin>>n;
+
+    cout<<"Enter the Number of Rectangale: ";
+    cin>>m;
+
+    Rectangle r[m];
+    Circle c[n];
+    int choice;
+    do{
+        cout<<"----------------------WELCOME-----------------------"<<endl;
+        cout<<"1. Circle Area "<<endl;
+        cout<<"2. Ractangle Area "<<endl;
+        cout<<"3. Input of Redius "<<endl;
+        cout<<"4. Input of Length and Breadth "<<endl;
+        cout<<"5. Exit"<<endl;
+
+        cout<<"Enter Choice "<<endl;
+        cin>>choice;
+
+        switch (choice)
+        {
+        case 1:
+                for(int i=0; i<n; i++){
+                    c[i].area();
+                }
+            break;
+        case 2:
+                for(int i=0; i<m; i++){
+                    r[i].area();
+                }
+                break;
+        case 3:
+                for(int i=0; i<n; i++){
+                    c[i].CircleInput();
+                }
+                break;
+        case 4:
+                for(int i=0; i<m; i++){
+                    r[i].RectangleInput();
+                }
+                break;        
+        case 5:
+                cout<<" EXIT "<<endl;
+                break;
+        default:
+                cout<<"Invelide Choice "<<endl;
+            break;
+            }    
+    }while(choice!=5);
+    
     return 0;
 }
