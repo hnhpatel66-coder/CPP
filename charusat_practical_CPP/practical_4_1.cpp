31c548f220cf4379658609a695bcaf9ec598ba79
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-04-09 09:56:00 +0530

    practical 4.1

--- charusat_practical_CPP/practical_4_1.cpp
+++ charusat_practical_CPP/practical_4_1.cpp
@@ -1,39 +1,135 @@
 #include <iostream>
 using namespace std;
+class dynamicarray
+{
+    int *arr;
+    int size;
 
-class dynamicarray{
-    private:
-        int *arr;
-        int size;
-    public:
-        dynamicarray(){
-            arr=0;
-            size=0;
+public:
+    dynamicarray()
+    {
+        arr = '\0';
+        size = 0;
+    }
+    void insternew(int val)
+    {
+
+        int *newarray = new int[size + 1];
+        for (int i = 0; i < size; i++)
+        {
+            newarray[i] = arr[i];
         }
-
-        void insertnew(int val){
-
+        newarray[size] = val;
+        delete[] arr;
+        arr = newarray;
+        size++;
+    }
+    void remove(int p)
+    {
+        if (size == 0)
+        {
+            cout << "Array is empty";
+            return;
         }
-
+        if (p < 0 || p >= size)
+        {
+            cout << "Invalid position"<<endl;
+            return;
+        }
+        int *newarray = new int[size - 1];
+        for (int i = 0, j = 0; i < size; i++)
+        {
+            if (i != p)
+            {
+                newarray[j++] = arr[i];
+            }
+        }
+        delete[] arr;
+        arr = newarray;
+        size--;
+    }
+    void insterat(int p, int val)
+    {
+        if (p < 0 || p > size)
+        {
+            cout << "Invalid position"<<endl;
+            return;
+        }
+        int *newarray = new int[size + 1];
+        for (int i = 0, j = 0; i < size + 1; i++)
+        {
+            if (i == p)
+            {
+                newarray[i] = val;
+            }
+            else
+            {
+                newarray[i] = arr[j++];
+            }
+        }
+        delete[] arr;
+        arr = newarray;
+        size++;
+    }
+    void serch(int val)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] == val)
+            {
+                cout << "number found at position: " << i << endl;
+                return;
+            }
+        }
+        cout << "number not found"<<endl;
+    }
+    void count(int val)
+    {
+        int count = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] == val)
+            {
+                count++;
+            }
+        }
+        cout << "Count of " << val << ": " << count << endl;
+    }
+    void display()
+    {
+        if (size == 0)
+        {
+            cout << "Array is empty"<<endl;
+            return;
+        }
+        cout << "Array: ";
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
 };
-
-int main() {
+int main()
+{
     dynamicarray c1;
-    c1.insternew(9);
-    c1.display();
-    c1.insternew(11);
+
+    int n;
+    cout<<"Enter value of N: ";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        c1.insternew(i * 10);
+    }
     c1.display();
     c1.insternew(33);
     c1.display();
-    c1.insternew(19);
+    c1.remove(7);
     c1.display();
-    c1.insternew(15);
+    c1.insterat(3, 33);
     c1.display();
-    c1.remove(17);
+    c1.serch(99);
     c1.display();
-    c1.insterat(3, 19);
-    c1.display();
-    c1.serch(39);
-    c1.count(91);
+    c1.count(19);
     return 0;
 }
--- charusat_practical_CPP/practical_7_6.cpp
+++ charusat_practical_CPP/practical_7_6.cpp
@@ -4,12 +4,12 @@ using namespace std;
 class Base {
 public:
     Base() {
-        cout << "Base Constructor Called" << endl;
+        cout<<"Base Constructor Called"<<endl;
     }
 
     //  Non-virtual destructor
     ~Base() {
-        cout << "Base Destructor Called" << endl;
+        cout<<"Base Destructor Called"<<endl;
     }
 };
 
@@ -18,20 +18,21 @@ class Derived : public Base {
 public:
     Derived() {
         data = new int[5]; // dynamic memory allocation
-        cout << "Derived Constructor Called" << endl;
+        cout<<"Derived Constructor Called"<<endl;
     }
 
     ~Derived() {
         delete[] data; // memory free
-        cout << "Derived Destructor Called" << endl;
+        cout<<"Derived Destructor Called"<<endl;
     }
 };
 
 int main() {
-    cout << "---- Without Virtual Destructor ----" << endl;
+    cout<<"---- Without Virtual Destructor ----"<<endl;
 
-    Base* obj = new Derived();  // polymorphism
-    delete obj;                //  Derived destructor NOT called
+    Base* ptr;
+    ptr = new Derived();  // polymorphism
+    delete ptr;                //  Derived destructor NOT called
 
     return 0;
 }
