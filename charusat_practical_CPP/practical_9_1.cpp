bb623985e450d450f2d88eacf0e6687f406d6ff3
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-04-23 09:43:36 +0530

    practical 9.1

--- charusat_practical_CPP/practical_9_1.cpp
+++ charusat_practical_CPP/practical_9_1.cpp
@@ -4,27 +4,37 @@ using namespace std;
 
 int main() {
     vector<int> v={11,22,33};
-
-    v.push_back(10);
-    v.push_back(20);
-    v.push_back(30);
     
-    // using vector through reverse iterator
+    int n;
+    cout<<"Enter n elements: ";
+    cin>>n;
+
+    for(int i=0; i<n; i++) {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    // cin>>n>>m>>k;
+    // v.push_back(n);
+    // v.push_back(m);
+    // v.push_back(k);
+    
     cout<<"Elements: ";
     for(int x : v){
         cout<<x<<" ";
     }
     cout<<endl;
 
-    cout<<"using vector Reversed Elements: ";  
-    for (auto t = v.rbegin(); t!= v.rend(); t++) {
+    // using vector through reverse iterator
+    cout<<"using vector Reversed Elements: "<<endl;  
+    for(auto t = v.rbegin(); t!= v.rend(); t++) {
         cout<<*t<<" ";
     }
     cout<<endl;
 
     //using manually reverse iterator
-    cout<<"Manually Reversed Elements: ";
-    for (int i = v.size() - 1; i >= 0; i--) {
+    cout<<"Manually Reversed Elements: "<<endl;
+    for(int i = v.size() - 1; i >= 0; i--) {
         cout<<v[i]<< " ";
     }
     return 0;
